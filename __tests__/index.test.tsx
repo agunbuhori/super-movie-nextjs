d3f6c4347cced15b852226b4b144722786bc12bd
agunbuhori <agun.buhori@gmail.com>
2021-08-23 05:56:31 +0700

    Testing updated

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,9 +1,12 @@
 import { shallow, ShallowWrapper } from "enzyme";
+import MovieItem from "../components/Movies/MovieItem";
 import Movies from "../components/Movies/Movies";
 import SearchInput from "../components/SearchInput";
+import { useFetch } from "../config/http";
+import { MovieResponse } from "../interfaces/Movie";
 import Index from '../pages/index'
 
-describe("With Enzyme", () => {
+describe("Rendering testing", () => {
   let wrapper: ShallowWrapper;
 
   beforeEach(() => {
@@ -13,5 +16,15 @@ describe("With Enzyme", () => {
   it("Rendering index components", () => {
     expect(wrapper.find(SearchInput)).toHaveLength(1);
     expect(wrapper.find(Movies)).toHaveLength(1);
+  });
+
+  it("Fetching data", () => {
+    useFetch('/', {params: {s: "Batman", page: 1}}, (response: MovieResponse) => {
+      if (response.Response === 'True') {
+        expect(response.Search.map((movie, index) => (
+          <MovieItem {...movie} key={index} showImage={() => {}}/>
+        ))).toContain(response.Search[0].Title);
+      }
+    });
   });
 });
